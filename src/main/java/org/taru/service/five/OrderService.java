fcc80b87e5b180be38fe66b00cb3b0e90c3aa6e1
1997XL <xl18167528300@163.com>
2019-08-21 20:43:55 +0800

    Delete OrderService.java

--- src/main/java/org/taru/service/five/OrderService.java
+++
@@ -1,8 +0,0 @@
-package org.taru.service.five;
-
-import java.text.ParseException;
-import java.util.Date;
-
-public interface OrderService {
-    int queryMoney(int homeId, String homeInDate, String homeOutDate) throws ParseException;
-}
