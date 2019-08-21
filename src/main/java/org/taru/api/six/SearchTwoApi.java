2d3f28a255fc292cc353b23a3fa20daae827f54a
liyupeng <2575566766@qq.com>
2019-08-21 20:42:22 +0800

    更新二级页面多条件查询

--- src/main/java/org/taru/api/six/SearchTwoApi.java
+++ src/main/java/org/taru/api/six/SearchTwoApi.java
@@ -5,10 +5,7 @@ import com.github.pagehelper.PageInfo;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.taru.entity.HomeLandlordStyleVO;
 import org.taru.service.six.SearchTwoInterface;
 import org.taru.vo.JsonResult;
@@ -16,6 +13,8 @@ import org.taru.vo.JsonResult;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 @Controller
 @CrossOrigin(allowCredentials = "true")
@@ -24,20 +23,22 @@ public class SearchTwoApi {
     SearchTwoInterface d;
     @RequestMapping(value = "/api/searchTwo",method = RequestMethod.GET)
     @ResponseBody
-    public JsonResult searchTwo(@Param("homeInDate")Date homeInDate,
-                                @Param("homeOutDate")Date homeOutDate,
-                                @Param("styleName")String styleName,
-                                @Param("homeAddress")String homeAddress,
-                                @Param("homeSpot")String homeSpot,
-                                @Param("pageNum")int pageNum,
-                                @Param("pageSize")int pageSize){
+    public JsonResult searchTwo(@RequestBody Map<String,Object> map){
+//        @Param("homeInDate")Date homeInDate,
+//        @Param("homeOutDate")Date homeOutDate,
+//        @Param("styleName")String styleName,
+//        @Param("homeAddress")String homeAddress,
+//        @Param("homeSpot")String homeSpot,
+//        @Param("pageNum")int pageNum,
+//        @Param("pageSize")int pageSize
+
         HashMap hashMap=new HashMap();
-        hashMap.put("homeInDate",homeInDate);
-        hashMap.put("homeOutDate",homeOutDate);
-        hashMap.put("styleName",styleName);
-        hashMap.put("homeAddress",homeAddress);
-        hashMap.put("homeSpot",homeSpot);
-        PageHelper.startPage(pageNum,pageSize);
+        hashMap.put("homeInDate",map.get("homeInDate"));
+        hashMap.put("homeOutDate",map.get("homeOutDate"));
+        hashMap.put("styleName",map.get("styleName"));
+        hashMap.put("homeAddress",map.get("homeAddress"));
+        hashMap.put("homeSpot",map.get("homeSpot"));
+        PageHelper.startPage((int)map.get("pageNum"),(int)map.get("pageSize"));
         List<HomeLandlordStyleVO> list=d.searchTwoDao(hashMap);
         PageInfo pageInfo=new PageInfo(list);
         JsonResult json=null;
