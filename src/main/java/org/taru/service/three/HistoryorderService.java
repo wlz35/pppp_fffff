1faa943fb5c968560739021a6ffd2b262540a261
b-demon <2421473203@qq.com>
2019-08-21 16:12:07 +0800

    Add files via upload
    添加房源、历史订单、单个订单详情、用户详情

---
+++ src/main/java/org/taru/service/three/HistoryorderService.java
@@ -0,0 +1,8 @@
+package org.taru.service.three;
+
+import org.taru.entity.Order;
+import org.taru.entity.User;
+
+public interface HistoryorderService {
+    User historyorder(int userId);
+}
---
+++ src/main/java/org/taru/service/three/QueryorderbyIdService.java
@@ -0,0 +1,7 @@
+package org.taru.service.three;
+
+import org.taru.entity.Order;
+
+public interface QueryorderbyIdService {
+    Order queryorder(int id);
+}
