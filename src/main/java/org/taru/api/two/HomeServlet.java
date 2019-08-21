ff05cd36aff68dd7ba248bbe8714a35afbb5f381
yourzirs1026 <yourzirs1026@qq.com>
2019-08-21 22:38:34 +0800

    Add files via upload

---
+++ src/main/java/org/taru/api/two/HomeServlet.java
@@ -0,0 +1,100 @@
+package org.taru.api.two;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.taru.service.two.HomeService;
+import org.taru.vo.JsonResult;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class HomeServlet {
+    @Autowired
+    HomeService homeService;
+    /**
+     *查询房源详细信息（包括房东、评论、配置）
+     * @return
+     */
+    @RequestMapping("/api/user/queryAllHomeInfo")
+    public JsonResult queryHomeInfo2() {
+        JsonResult jsonResult = null;
+        try {
+            List home = homeService.queryHomeInfo1();
+            if (home != null) {
+                jsonResult = new JsonResult("200", "查询房源详细信息（包括房东、评论、配置）成功", home);
+            } else {
+                jsonResult = new JsonResult("400", "查询房源详细信息（包括房东、评论、配置）失败", null);
+            }
+        } catch (Exception e) {
+            jsonResult = new JsonResult("500", "", e.getMessage());
+        }
+        return jsonResult;
+    }
+
+    /**
+     * 查询所有房源风格
+     * @return
+     */
+    @RequestMapping("/api/user/queryHomeStyle")
+    public JsonResult queryHomeStyle2(){
+        JsonResult jsonResult = null;
+        try {
+            List home = homeService.queryHomeStyle1();
+            if (home != null) {
+                jsonResult = new JsonResult("200", "查询房源风格成功", home);
+            } else {
+                jsonResult = new JsonResult("400", "查询房源风格失败", null);
+            }
+        } catch (Exception e) {
+            jsonResult = new JsonResult("500", "", e.getMessage());
+        }
+        return jsonResult;
+    }
+
+    /**
+     * 根据ID查询房源详细信息
+     * @param homeId
+     * @return
+     */
+    @RequestMapping("/api/user/queryHomeInfoById")
+    public JsonResult queryHomeInfoById2(String homeId){
+        JsonResult jsonResult = null;
+        try{
+            List home = homeService.queryHomeInfoById1(homeId);
+            if(home != null){
+                jsonResult = new JsonResult("200","根据ID查询房源详细信息成功",home);
+            }else {
+                jsonResult = new JsonResult("400","根据ID查询房源详细信息失败",null);
+            }
+        }catch (Exception e){
+            jsonResult = new JsonResult("500","服务器异常",e.getMessage());
+        }
+        return jsonResult;
+    }
+
+    /**
+     * 根据房源ID房源图片（5张），房东信息，房东头像，评论信息
+     * @param homeId
+     * @return
+     */
+    @RequestMapping("/api/user/query/HomeById")
+    public JsonResult queryHomeById2(String homeId){
+        JsonResult jsonResult = null;
+        try{
+            List home = homeService.queryHomeById1(homeId);
+            if(home != null){
+                jsonResult = new JsonResult("200","根据房源ID房源图片（5张），房东信息，房东头像，评论信息成功",home);
+            }else {
+                jsonResult = new JsonResult("400","根据房源ID房源图片（5张），房东信息，房东头像，评论信息失败",null);
+            }
+        }catch (Exception e){
+            jsonResult = new JsonResult("500","服务器异常",e.getMessage());
+        }
+        return jsonResult;
+    }
+
+}
---
+++ src/main/java/org/taru/api/two/OrderServlet.java
@@ -0,0 +1,35 @@
+package org.taru.api.two;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.taru.vo.two.Order;
+import org.taru.service.two.OrderService;
+import org.taru.vo.JsonResult;
+
+@RestController
+public class OrderServlet {
+    @Autowired
+    OrderService orderService;
+
+    /**
+     * 插入订单
+     * @param order
+     * @return
+     */
+    @RequestMapping("/api/order/commit1")
+    public JsonResult insertOrderCommit2(Order order){
+        JsonResult jsonResult = null;
+        try{
+            int order1 = orderService.insertOrderCommit1(order);
+            if(order1 != 0){
+                jsonResult = new JsonResult("200","提交订单成功",order1);
+            }else {
+                jsonResult = new JsonResult("400","提交订单失败",null);
+            }
+        }catch (Exception e){
+            jsonResult = new JsonResult("500","服务器异常", e.getMessage());
+        }
+        return jsonResult;
+    }
+}
