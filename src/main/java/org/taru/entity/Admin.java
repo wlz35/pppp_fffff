023dbb5f359de320a6129d68e95019b391d19c57
wlz35 <458954355@qq.com>
2019-08-21 17:41:55 +0800

    Add files via upload

---
+++ src/main/java/org/taru/entity/Admin.java
@@ -0,0 +1,32 @@
+package org.taru.entity;
+
+public class Admin {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String name;
+    private String password;
+}
