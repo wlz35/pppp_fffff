e20cabd9ba33f712ccc69025e1d32a882eed76c3
b-demon <2421473203@qq.com>
2019-08-21 15:20:09 +0800

    Delete AddHomeDao.java

--- AddHomeDao.java
+++
@@ -1,9 +0,0 @@
-package org.taru.dao.three;
-
-import org.apache.ibatis.annotations.Mapper;
-@Mapper
-public interface AddHomeDao {
-    int addhome(String styleName,String homeName,String homeAddress
-            ,String homePrice,String home_in_Date,String home_out_Date,String picturesId,String homeDescribe,String homeSpot,String homeConfigure,
-                String landlordPhone);
-}
