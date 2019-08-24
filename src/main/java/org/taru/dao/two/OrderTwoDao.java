3ea4993befe107a4800f2e8ab139098d5b0ff91b
yourzirs1026 <yourzirs1026@qq.com>
2019-08-24 17:32:51 +0800

    update orderDao

---
+++ src/main/java/org/taru/dao/two/OrderTwoDao.java
@@ -0,0 +1,17 @@
+package org.taru.dao.two;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+import org.taru.vo.two.Order;
+
+@Mapper
+@Component("orderMapper")
+public interface OrderTwoDao {
+
+    /**
+     * 插入订单
+     * @param order
+     * @return
+     */
+    int insertOrderCommit(Order order);
+}
--- src/main/java/org/taru/service/two/impl/OrderServiceImpl.java
+++ src/main/java/org/taru/service/two/impl/OrderServiceImpl.java
@@ -2,14 +2,14 @@ package org.taru.service.two.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.taru.dao.two.OrderDao;
+import org.taru.dao.two.OrderTwoDao;
 import org.taru.vo.two.Order;
 import org.taru.service.two.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Autowired
-    OrderDao orderDao;
+    OrderTwoDao orderTwoDao;
 
     /**
      * 插入订单
@@ -18,6 +18,6 @@ public class OrderServiceImpl implements OrderService {
      */
     @Override
     public int insertOrderCommit1(Order order) {
-        return orderDao.insertOrderCommit(order);
+        return orderTwoDao.insertOrderCommit(order);
     }
 }
