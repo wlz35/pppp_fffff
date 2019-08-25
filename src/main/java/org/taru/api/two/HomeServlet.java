93024e563edaaa092be4bfca58b931ed7fd8a68a
yourzirs1026 <yourzirs1026@qq.com>
2019-08-25 09:28:55 +0800

    update orderDao

--- src/main/java/org/taru/api/two/HomeServlet.java
+++ src/main/java/org/taru/api/two/HomeServlet.java
@@ -1,17 +1,16 @@
 package org.taru.api.two;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.taru.service.two.HomeService;
 import org.taru.vo.JsonResult;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class HomeServlet {
     @Autowired
     HomeService homeService;
--- src/main/java/org/taru/api/two/OrderServlet.java
+++ src/main/java/org/taru/api/two/OrderServlet.java
@@ -1,6 +1,7 @@
 package org.taru.api.two;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.taru.vo.two.Order;
@@ -8,6 +9,7 @@ import org.taru.service.two.OrderService;
 import org.taru.vo.JsonResult;
 
 @RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class OrderServlet {
     @Autowired
     OrderService orderService;
