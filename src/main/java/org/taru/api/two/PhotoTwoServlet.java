9e0a7d3d20966b808e80cd6196f812ae9faf6268
yourzirs1026 <yourzirs1026@qq.com>
2019-08-25 22:58:44 +0800

    updatePHOTO

--- src/main/java/org/taru/api/two/PhotoTwoServlet.java
+++ src/main/java/org/taru/api/two/PhotoTwoServlet.java
@@ -1,6 +1,7 @@
 package org.taru.api.two;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.taru.service.two.PhotoTwoService;
@@ -9,6 +10,7 @@ import org.taru.vo.two.Pictures;
 
 
 @RestController
+@CrossOrigin
 public class PhotoTwoServlet {
     @Autowired
     PhotoTwoService photoTwoService;
