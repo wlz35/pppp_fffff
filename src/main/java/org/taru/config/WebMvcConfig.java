2b284fd190e50978dcb4c73223e1b91a6844f829
liyupeng <2575566766@qq.com>
2019-08-26 10:55:18 +0800

    添加了Swagger

---
+++ src/main/java/org/taru/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package org.taru.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 解决 swagger-ui.html 404报错
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+                }
+                }
