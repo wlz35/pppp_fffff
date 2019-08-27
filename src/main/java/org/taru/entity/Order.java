17eef63e3cf644c737707026d8ddf7022cb68e36
wlz <458954355@qq.com>
2019-08-27 18:46:18 +0800

    订单查询更新

--- src/main/java/org/taru/entity/Order.java
+++ src/main/java/org/taru/entity/Order.java
@@ -9,6 +9,43 @@ public class Order implements Serializable {
     private int orderId;
     private  int homeId;
     private User user;
+    private String homeName;
+
+    public String getHomeName() {
+        return homeName;
+    }
+
+    public void setHomeName(String homeName) {
+        this.homeName = homeName;
+    }
+
+    public String getHomePrice() {
+        return homePrice;
+    }
+
+    public void setHomePrice(String homePrice) {
+        this.homePrice = homePrice;
+    }
+
+    public String getPicTure() {
+        return picTure;
+    }
+
+    public void setPicTure(String picTure) {
+        this.picTure = picTure;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    private String homePrice;
+    private String picTure;
+    private String homeAddress;
 
     public User getUser() {
         return user;
