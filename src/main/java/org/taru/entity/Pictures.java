c5245fa42c59c30fe1255dbfc3cd40ef225f7746
wlz <458954355@qq.com>
2019-08-27 11:22:28 +0800

    实体图片

---
+++ src/main/java/org/taru/entity/Pictures.java
@@ -0,0 +1,61 @@
+package org.taru.entity;
+
+public class Pictures {
+    private int pictureId;
+
+    public int getPictureId() {
+        return pictureId;
+    }
+
+    public void setPictureId(int pictureId) {
+        this.pictureId = pictureId;
+    }
+
+    public String getPictureOne() {
+        return pictureOne;
+    }
+
+    public void setPictureOne(String pictureOne) {
+        this.pictureOne = pictureOne;
+    }
+
+    public String getPictureTwo() {
+        return pictureTwo;
+    }
+
+    public void setPictureTwo(String pictureTwo) {
+        this.pictureTwo = pictureTwo;
+    }
+
+    public String getPictureThree() {
+        return pictureThree;
+    }
+
+    public void setPictureThree(String pictureThree) {
+        this.pictureThree = pictureThree;
+    }
+
+    public String getPictureFour() {
+        return pictureFour;
+    }
+
+    public void setPictureFour(String pictureFour) {
+        this.pictureFour = pictureFour;
+    }
+
+    public String getPictureFive() {
+        return pictureFive;
+    }
+
+    public void setPictureFive(String pictureFive) {
+        this.pictureFive = pictureFive;
+    }
+
+    private String pictureOne;
+    private String pictureTwo;
+    private String pictureThree;
+    private String pictureFour;
+    private String pictureFive;
+
+
+}
